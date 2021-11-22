a1278bc43e90bdb75e2f1d7bb3b70eeb8cd0c6d7
tark_lfs@hotmail.com <tark_lfs@hotmail.com>
2021-11-22 18:20:59 +0300

    Game OverUI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,19 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using TMPro;
+using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
 
-
+   
     public List<TargetIndex> targetsIndex = new List<TargetIndex>();
     public List<GameObject> targets;
-    public float spawnRate=1.0f;
-    public TextMeshProUGUI scoreText;
-   private int score;
+    public float spawnRate = 1.0f;
+    public GameObject scoreText;
+    //public TextMeshProUGUI scoreText;
+    private int score;
+    public GameObject gameOverPanel;
+    bool isGameActive=true;
+     #region Singleton
+
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
     void Start()
-    {   StartCoroutine(nameof(SpawnTarget));
-    score=0;
-    scoreText.text="Score:"+score;
+    {     Time.timeScale=1;
+        StartCoroutine(nameof(SpawnTarget));
+        score = 0;
+        isGameActive=true;
         // targetsIndex.Single(s => s.name=="Ob");
         //GameObject target=ObjectPooler.SharedInstance.GetPooledObject(targetsIndex.Single(s => s.name=="Good1").index);
 
@@ -27,12 +51,40 @@ public class GameManager : MonoBehaviour
 
     }
     IEnumerator SpawnTarget()
-    {   while(true){
-        GameObject target = ObjectPooler.SharedInstance.GetPooledObject(Random.Range(0, targetsIndex.Count));
-        if (target != null) target.SetActive(true);
-        yield return new WaitForSeconds(spawnRate);
+    {
+        while (isGameActive)
+        {
+            GameObject target = ObjectPooler.SharedInstance.GetPooledObject(Random.Range(0, targetsIndex.Count));
+            if (target != null) {
+            target.SetActive(true);  
+           
+            }
+            
+            yield return new WaitForSeconds(spawnRate);
 
+        }
     }
+    public void UpdateScore(int scoreToAdd)
+    {  
+        score += scoreToAdd;
+        scoreText.GetComponent<TextMeshProUGUI>().text = "Score:"+score;
+    
     }
+    public void GameOver(){
+      gameOverPanel.SetActive(true);
+      isGameActive=false;
+      scoreText.SetActive(false);
+      Time.timeScale=0;
+    }
+    public void TryAgain(){
+        gameOverPanel.SetActive(false);
+         scoreText.SetActive(true);
+         SceneManager.LoadScene("Prototype 5");
+        //relod sceen
+     }
+    public void Exit(){
+        //exit
+    }
+
 }
 
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,21 +6,34 @@ using Random = UnityEngine.Random;
 
 public class Target : MonoBehaviour
 {
-    Rigidbody targetRb;
-    public float xRange=1.95f;
+    private Rigidbody targetRb;
+    public float xRange = 1.95f;
     public float minSpeed = 14;
     public float maxSpeed = 16;
     public float maxTorque = 10;
-    public float ySpawnPos=-2;
-    void Start()
+    public float ySpawnPos = -2;
+    public int pointValue = 1;
+    public int effectIndex = 3;
+    private void Awake()
     {
         targetRb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+
+        //SetPositionAndForce();
+    }
+    private void OnEnable()
+    {
+        Debug.Log("On enabled");
+
         SetPositionAndForce();
     }
 
-    private void SetPositionAndForce()
+    public void SetPositionAndForce()
     {
         transform.position = new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+        targetRb.velocity = Vector3.zero;
         targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
 
         targetRb.AddTorque(Random.RandomRange(-maxTorque, maxTorque),
@@ -28,14 +41,38 @@ public class Target : MonoBehaviour
          Random.RandomRange(-maxTorque, maxTorque), ForceMode.Impulse);
     }
 
-    private void OnMouseDown() {
+    private void OnMouseDown()
+    {   
+        if(gameObject.CompareTag("Bad")) GameManager.Instance.GameOver();
+        GameObject particleSystem = ObjectPooler.SharedInstance.GetPooledObject(effectIndex);
+        if (particleSystem!=null)
+        {
+            particleSystem.transform.position = transform.position;
+            
+            StartCoroutine(SetEffectTime(particleSystem));
+        }
+
+
+        //   particleSystem.SetActive(false);
         gameObject.SetActive(false);
+        GameManager.Instance.UpdateScore(pointValue);
+
     }
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("Sensor")) gameObject.SetActive(false);
+
+    IEnumerator SetEffectTime(GameObject go)
+    {  go.SetActive(true);
+        yield return new WaitForSeconds(1);
+        go.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.CompareTag("Sensor")) 
+        if(gameObject.CompareTag("Bad")) GameManager.Instance.GameOver();
+        gameObject.SetActive(false);
     }
     void Update()
     {
-
+        if (transform.position.y < -20) gameObject.SetActive(false);
     }
 }
